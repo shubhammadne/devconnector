78c46305178c14f373cd72e13c0bc78dfd1bb96d
Shubham Madne <madneshubham124@gmail.com>
2022-08-03 14:43:42 +0530

    replaced config library with dotenv library

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,10 @@
+require("dotenv").config()
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const auth = require('../../middleware/auth');
 const jwt = require('jsonwebtoken');
-const config = require('config');
+// const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -61,7 +62,7 @@ router.post(
 
       jwt.sign(
         payload,
-        config.get('jwtSecret'),
+        process.env.jwtSecret,
         { expiresIn: '5 days' },
         (err, token) => {
           if (err) throw err;
